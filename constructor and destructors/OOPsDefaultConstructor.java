b7836964e3b71806493cabb3c2284111d5a91bea
KRUSHI MONPARA <84612397+Krushi24112002@users.noreply.github.com>
2022-03-05 08:49:44 -0500

    Add files via upload

--- constructor and destructors/OOPsDefaultConstructor.java
+++ constructor and destructors/OOPsDefaultConstructor.java
@@ -10,30 +10,40 @@ objects like null, 0, etc. according to the type.
 1.A constructor’s name must be exactly the same as the name of its class.
 2.The constructor is a special method because it does not have a return type. We
 do not even need to write void as the return type
+3.It can have all Access Modifiers are allowed (private, public, protected, default)
+4.It Cannot have any Non Access Modifiers (final, static, abstract, synchronized)
+5.It can take any number of parameters
 */
 
 // NOTE:constructor is called as soon as the object is created as constructor initializes the object.
+
+// Default Constructor
+// NOTE: A constructor is called a "Default Constructor" when it doesn't have any parameter
+// NOTE: A constructor is just like a method(Function) in Java, but it does not have any return type. It can also be overloaded, just like other methods.
+
 import java.util.*;
 
-public class OOPsDefaultConstructor {
+// Create A Class
+class Person {
 
-    // Create A Class
-    static class Person {
+    // Create Class Properties
+    int age;
+    String name;
 
-        // Create Class Properties
-        int age;
-        String name;
-
-        // Create Class Function
-        void PersonDetail() {
-            System.out.println(age + " " + name);
-        }
+    // Create Class Function
+    void PersonDetail() {
+        System.out.println("Your Age Is " + age + " " + "Your Name Is " + name);
     }
+}
+
+public class OOPsDefaultConstructor {
 
     public static void main(String[] args) {
 
         // Creating Object
         Person FirstPerson = new Person();
+        // NOTE: Current Can Not Add Any Properties Value That Reson Can Not Show Any
+        // Age And Name Value. Print Data Type Default Value.
         FirstPerson.PersonDetail();
 
     }
