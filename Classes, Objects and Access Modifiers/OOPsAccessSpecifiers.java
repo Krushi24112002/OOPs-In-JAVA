4f25b0567f88803570c0db3c716cae31619068c0
KRUSHI MONPARA <84612397+Krushi24112002@users.noreply.github.com>
2022-03-05 08:49:02 -0500

    Add files via upload

--- Classes, Objects and Access Modifiers/OOPsAccessSpecifiers.java
+++ Classes, Objects and Access Modifiers/OOPsAccessSpecifiers.java
@@ -1,40 +1,40 @@
 import java.util.*;
 
-public class OOPsAccessSpecifiers {
+// Create Class
+class Person {
 
-    // Create Class
-    static class Person {
+    // Create Properties
 
-        // Create Properties
+    // Private Properties
+    private int age;
+    // Public Properties
+    public String name;
 
-        // Private Properties
-        private int age;
-        // Public Properties
-        public String name;
-
-        // Method-1 Getter
-        int getage() {
-            return age;
-        }
-
-        // Methode-2 Setter
-        void setage(int n) {
-
-            // This Keyword Refers To Current instance Itself
-            this.age = n;
-            // OR
-            // age = n; This Type Can Also Write
-        }
-
-        // This Function Use Direct Assecc Not Use Any Getter or Setter Function
-        void PersonName() {
-            System.out.println(name);
-        }
-
+    // Method-1 Getter
+    int getage() {
+        return age;
     }
 
+    // Methode-2 Setter
+    void setage(int n) {
+
+        // This Keyword Refers To Current instance Itself
+        this.age = n;
+        // OR
+        // age = n; This Type Can Also Write
+    }
+
+    // This Function Use Direct Assecc Not Use Any Getter or Setter Function
+    void PersonName() {
+        System.out.println("Your Name Is " + name);
+    }
+
+}
+
+public class OOPsAccessSpecifiers {
+
     public static void main(String[] args) {
-        
+
         // Create Object Using New Keyword
         Person FirstPerson = new Person();
 
@@ -43,7 +43,7 @@ public class OOPsAccessSpecifiers {
         // Getting The Name By Calling Getter Function
         System.out.println(FirstPerson.getage());
 
-        FirstPerson.name = "KRUSHI";
+        FirstPerson.name = "KRUSHI MONPARA";
         FirstPerson.PersonName();
 
     }
