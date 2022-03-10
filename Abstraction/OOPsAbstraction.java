507494dd3b8f87042bb6f24b06626b6c10966ac1
KRUSHI MONPARA <84612397+Krushi24112002@users.noreply.github.com>
2022-03-10 11:35:40 -0500

    Add files via upload

---
+++ Abstraction/OOPsAbstraction.java
@@ -0,0 +1,8 @@
+// Abstraction in java(OOPs)
+// Defintion:- Data Abstraction is The Process Of Hiding Certain Details And Showing Only Essential Information To The User.
+/* Advantages Of Abstraction
+1. Only you can make changes to your data or function, and no one else can.
+2. Increases the reusability of the code.
+3. Avoids duplication of your code.
+4. It makes the application secure by not allowing anyone else to see the Background details.
+*/
